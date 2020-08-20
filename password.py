98f11451437e962fd6d8a8253714ddec61dd1460
Gary Hsu <garyhsu0422@gmail.com>
2020-08-20 12:04:11 +0800

    basic version finished

---
+++ password.py
@@ -0,0 +1,13 @@
+password = 'gary1234'
+x = 3
+while True:
+	pwd = input('請輸入密碼： ')
+	if pwd == password:
+		print('登入成功')
+		break
+	else:
+		x = x - 1
+		print('密碼錯誤！你還有',  x,'次機會')
+	if x <= 0:
+		print('你沒機會了')
+		break
